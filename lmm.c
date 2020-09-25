4b9288331c2a000cc5dff23c6e640470f0181ca7
Hahwee <hql005@ucsd.edu>
2020-09-25 11:18:53 -0700

    added mmap and munmap functions

--- lmm.c
+++ lmm.c
@@ -2,7 +2,41 @@
 #include <memory.h>
 #include <unistd.h> //getpagesize() function
 #include <sys/mman.h> //mmap()
-#include "lmm.h"
 
+//static variables
+static size_t PAGE_SIZE=0;
 
+//function to map virtual memory page size to memory and return pointer to beginning region of allocated memory
+static void * get_VMpage_from_kernel(int units){
+	void * vmpage=mmap(0,PAGE_SIZE*units,PROT_EXEC|PROT_WRITE|PROT_READ,MAP_PRIVATE|MAP_ANONYMOUS,0,0);	
+	printf("%p\n",vmpage);
+
+	if (vmpage==MAP_FAILED){
+		printf("Unable to get page\n");
+		return NULL;
+	}
+
+	return vmpage;
+}
+
+//function to unmap virual memory page/s from memory and returns success/fail value
+static void return_VMpage_to_kernel(void * vmPage, int units){
+	int flagValue=munmap(vmPage,units);
+	printf("%d\n",flagValue);
+	if (flagValue==-1)
+		printf("Failed to unmap memory\n");
+	else if (flagValue==0)
+		printf("Successfully unmapped memory\n");
+}
+
+int main(){
+	//function to getpagesize of virtual memory page on Linux system and print
+	PAGE_SIZE=getpagesize();
+	printf("%ld\n",PAGE_SIZE);
+
+	void * vmpage_Address=get_VMpage_from_kernel(2);
+	return_VMpage_to_kernel(vmpage_Address, PAGE_SIZE*0);
+
+	return 0;
+}
 
